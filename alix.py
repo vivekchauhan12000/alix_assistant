005b6f209c413c7955279d6c11abd416c60836e0
vivekchauhan12000 <49834398+vivekchauhan12000@users.noreply.github.com>
2020-09-03 18:47:38 +0530

    voice_notadded

--- alix.py
+++ alix.py
@@ -1,8 +1,26 @@
 import speech_recognition as sr
+print('it work')
 
 r = sr.Recognizer()
-with sr.Microphone() as source:
-   print("say something")
-   audio = r.listen(source)
-   voice_data = r.recognize_google(audio)
-   print(voice_data)
+def record_audio():
+ with sr.Microphone() as source:
+    audio = r.listen(source)
+    voice_data=''
+    try:
+         voice_data = r.recognize_google(audio)
+         
+    except sr.UnknownValueError:
+        print("sorry, I did not get that")
+    except sr.RequestError:
+        print("speech is dwon today")   
+    return voice_data
+
+def respond(voice_data):
+   if 'name' == voice_data:
+      print("My name is alix")
+
+print("how can i help you")        
+voice_data = record_audio()
+print("me:" + voice_data)
+respond(voice_data)
+
